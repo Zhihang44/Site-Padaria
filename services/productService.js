dbaa12dae256cf5b415a040a1eb13bd407e704c9
Zhihang44 <zhihang.00000844596@unicap.br>
2025-12-16 13:42:55 -0300

    refactor: enhance product creation and deletion error handling

--- services/productService.js
+++ services/productService.js
@@ -3,6 +3,10 @@ const { ERROR_MESSAGES } = require('../utils/utils');
 
 const createProduct = async (productData) => {
     try {
+        const product = await Product.findOne({ where: { name: productData.name } });
+        if (product) {
+            throw new Error(ERROR_MESSAGES.PRODUCT_ALREADY_EXISTS);
+        }
         const newProduct = await Product.create(productData);
         return newProduct;
     } catch (error) {
@@ -12,7 +16,11 @@ const createProduct = async (productData) => {
 
 const deleteProduct = async (id) => {
     try {
+
         const product = await Product.findByPk(id);
+        if (!product) {
+            throw new Error(ERROR_MESSAGES.PRODUCT_NOT_FOUND);
+        }
         await product.destroy();
         return true;
     } catch (error) {
